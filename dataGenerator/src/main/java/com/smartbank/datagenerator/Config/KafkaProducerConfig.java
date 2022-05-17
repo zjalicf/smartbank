36195477326ea3f60947ce4dd8c41769e4e4accb
zjalicf <64900037+zjalicf@users.noreply.github.com>
2022-05-17 12:59:34 +0200

    Data generator (#2)
    Data generator service

---
+++ dataGenerator/src/main/java/com/smartbank/datagenerator/Config/KafkaProducerConfig.java
@@ -0,0 +1,54 @@
+package com.smartbank.datagenerator.Config;
+
+import com.smartbank.datagenerator.Model.Account;
+import com.smartbank.datagenerator.Model.Transaction;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+
+@Configuration
+@EnableKafka
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ProducerFactory<String, Account> accountProducerFactory() {
+        HashMap<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public ProducerFactory<String, Transaction> transactionProducerFactory() {
+        HashMap<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Account> accountKafkaTemplate() {
+        return new KafkaTemplate<>(accountProducerFactory());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Transaction> transactionKafkaTemplate() {
+        return new KafkaTemplate<>(transactionProducerFactory());
+    }
+}
---
+++ dataGenerator/src/main/java/com/smartbank/datagenerator/DataGeneratorApplication.java
@@ -0,0 +1,17 @@
+package com.smartbank.datagenerator;
+
+import com.smartbank.datagenerator.Service.DataGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGeneratorApplication {
+
+    @Autowired
+    DataGenerator dataGenerator;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataGeneratorApplication.class, args);
+    }
+}
