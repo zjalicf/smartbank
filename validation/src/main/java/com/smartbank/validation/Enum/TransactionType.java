e32b818b2bbf6dd2756d9b0c425dd21ca544e3cf
zjalicf <zjalicf@gmail.com>
2022-05-20 13:48:01 +0200

    removed junk

--- validation/src/main/java/com/smartbank/validation/Enum/TransactionType.java
+++
@@ -1,6 +0,0 @@
-package com.smartbank.validation.Enum;
-
-public enum TransactionType {
-    WITHDRAW,
-    DEPOSIT
-}
--- validation/src/main/java/com/smartbank/validation/Repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartbank.validation.Repository;
-
-import com.smartbank.validation.Model.Account;
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-import java.util.UUID;
-
-public interface AccountRepository extends CassandraRepository<Account, UUID> {
-}
